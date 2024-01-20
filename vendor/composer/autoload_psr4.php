d5179d89463ad0efcb3eee03acd1fe5c30cfc6f5
Jose Lustosa <janeto29@gmail.com>
2024-01-20 14:18:11 -0300

    atualizado

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    '' => array($vendorDir . '/janeto29/php-classes/src'),
+    'Hcode\\' => array($vendorDir . '/hcodebr/php-classes/src'),
 );
